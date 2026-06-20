048c05491796777eda3735abdd1f074daf033dca
mareerray <mareerray@gmail.com>
2026-06-21 02:37:09 +0300

    feat: comment out server proxy configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.football-data.org',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://api.football-data.org',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // },
   
 })
